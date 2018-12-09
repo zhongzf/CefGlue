11268fe1cf32da92a8589bd041b3a5f6ed77d8af
Dmitry Azaraev <dmitry.azaraev@gmail.com>
2018-12-09 23:19:32 +0300

    Update to 3.3538.1852.gcb937fc (Chromium 70.0.3538.102) (No actual changes.)

--- CefGlue/Interop/version.g.cs
+++ CefGlue/Interop/version.g.cs
@@ -9,15 +9,15 @@ namespace Xilium.CefGlue.Interop
     
     internal static unsafe partial class libcef
     {
-        public const string CEF_VERSION = "3.3538.1849.g458cc98";
+        public const string CEF_VERSION = "3.3538.1852.gcb937fc";
         public const int CEF_VERSION_MAJOR = 3;
-        public const int CEF_COMMIT_NUMBER = 1849;
-        public const string CEF_COMMIT_HASH = "458cc9859087f72ddc940aa4bc4f9371626d7ad1";
+        public const int CEF_COMMIT_NUMBER = 1852;
+        public const string CEF_COMMIT_HASH = "cb937fc9cd02a5b5f4d5aea2d880e05fbab621bf";
         
         public const int CHROME_VERSION_MAJOR = 70;
         public const int CHROME_VERSION_MINOR = 0;
         public const int CHROME_VERSION_BUILD = 3538;
-        public const int CHROME_VERSION_PATCH = 77;
+        public const int CHROME_VERSION_PATCH = 102;
         
         public const string CEF_API_HASH_UNIVERSAL = "040ce5036de742f2e20741ea315f8943acb52825";
         
